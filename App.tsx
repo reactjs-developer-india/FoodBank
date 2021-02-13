885546f820aab784d4d5bc66f2b26999c80dea6e
Nawab <nawab.mir14@gmail.com>
2021-02-13 13:41:05 +0000

    added volunteer map

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DonateOrVolunteer from "./features/DonateOrVolunteer";
 import PostcodeAndName from "./features/PostcodeAndName";
+import VolunteerMap from "./features/VolunteerMap";
 
 export default function App() {
   const [page, setPage] = useState("DonateOrVolunteer");
@@ -9,6 +10,7 @@ export default function App() {
     <>
       {page === "DonateOrVolunteer" && <DonateOrVolunteer setPage={setPage} />}
       {page === "DonateMain" && <PostcodeAndName setPage={setPage} />}
+      {page === "VolunteerMain" && <VolunteerMap setPage={setPage} />}
     </>
   );
 }
